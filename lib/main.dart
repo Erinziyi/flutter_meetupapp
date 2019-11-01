c90a0b770eb04b7a18339d2386f4944840738dce
unknown <erin@peoplelogy.com>
2019-11-01 10:58:39 +0800

    fist meetup

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MeetuperApp());
+
+class MeetuperApp extends StatelessWidget {
+  final String appTitle = 'Meetuper App';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue),
+    home: Scaffold(
+    body: Center(
+    child: Text(
+    'Welcome in $appTitle!',
+    textDirection: TextDirection.ltr
+     ),
+    ),
+    appBar: AppBar(title: Text(appTitle)),
+    )
+
+    );
+  }
+}
+
+
+
